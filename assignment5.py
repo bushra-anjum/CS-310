c7b54431e0110106c8b9d147ae0dca85ce23d264
bushra-anjum <bushra.sumbal999@gmail.com>
2019-10-04 22:43:38 +0500

    third commit

---
+++ assignment5.py
@@ -0,0 +1,17 @@
+x=6
+print(x)
+y=x+7
+print(y)
+ 
+ print('Hello World')
+ print('You must be the legendary god that comes from the sky')
+ print('We have been waiting for you for a long time')
+ print('Our legends say that you will be very tasty for mustard')
+ print('We will have a feast tonight unless you say')
+ 
+ x=43
+ x=x+1
+ print(x)
+ print('I hate you python')
+ print('if you dont mind i need to leave')
+ print('good bye')
