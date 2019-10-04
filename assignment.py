879b5c2f8a717e8c9c7dc24dea18a50860ab9f0b
bushra-anjum <bushra.sumbal999@gmail.com>
2019-10-04 21:02:06 +0500

    Second Commit

---
+++ assignment.py
@@ -0,0 +1,5 @@
+print("Hello world")
+print("You must be the legendary god that comes from the sky")
+print("We have been waiting for for a long time")
+print("Our legend says you will be very tasty with the mustard")
+print("We will have a feast tonight unless you say")
