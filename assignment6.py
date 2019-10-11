2237acf94e0f0fe3509a01268d2a805add0dae0f
bushra-anjum <bushra.sumbal999@gmail.com>
2019-10-12 01:26:29 +0500

    My fourth commit

---
+++ assignment6.py
@@ -0,0 +1,72 @@
+print(4)
+ 
+ type('Hello World')
+ type('17')
+ type('3.2')
+ 
+ print('1,000,000')
+ 
+ message = 'And now for something completely different'
+ n = 17
+ pi = 3.1415926535897931
+ print(n)
+ print(pi)
+ print(message)
+ 
+ print(1)
+ x = 2
+ print(x)
+ 
+ minute = 59
+ minute/60
+ 
+ 5
+ x = 5
+ x+1
+ 
+ qoutient = 7//3
+ print(qoutient)
+ remainder = 7%3
+ print(remainder)
+ 
+ first = 10
+ second = 15
+ print(first+second)
+ 
+ first = '100'
+ second = '150'
+ print(first=second)
+ 
+ first = 'Test'
+ second = 3
+ print(first * second)
+ 
+ inp = input()
+ some silly stuff
+ print(inp)
+ 
+ name = input('what is your name?\n')
+ Chuck
+ print(name)
+ 
+ propmt = 'what...is the airspeed velocity of an unladen swallow?\n'
+ speed = input(propmt)
+ 17
+ int(speed)
+ int(speed) + 5
+ 
+ #compute the percentage of hour has elapsed
+ percentage = (minute * 100) / 60     #percentageof an hour
+ 
+ a = 35.0
+ b = 12.50
+ c = a * b
+ print(c)
+ 
+ hours = 35.0 
+ rate = 12.50 
+ pay = hours * rate
+ print(pay)
+ 
+ 
+ 
